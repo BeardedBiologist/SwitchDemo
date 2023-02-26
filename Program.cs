6deab01de4f9b87104dabed062df84b19518b303
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 22:13:38 +0100

    added in switch statement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 ﻿
+//DEMO DATA
+string firstName = "Josh";
+int age = 34;
 
-
-
-
+switch (firstName.ToLower())
+{
+    case "josh":
+        Console.WriteLine("Welcome back, Josh");
+        break;
+    case "tim":
+        Console.WriteLine("Hello Tim");
+        break;
+    default:
+        Console.WriteLine("I don't think I know you");
+        break;
+}
